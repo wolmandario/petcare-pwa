d449f04778bd1329f1bb54080f3a1b30d15d3287
wolmandario <wolmandario@gmail.com>
2025-10-18 22:48:35 -0300

    Add files via upload

---
+++ pwa-serviceworker.js
@@ -0,0 +1,33 @@
+// Service Worker para PWA
+const CACHE_NAME = 'petcare-v5.0.0-fixed';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+// Instalar Service Worker
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Interceptar requests
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Devolver desde cache si existe
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
